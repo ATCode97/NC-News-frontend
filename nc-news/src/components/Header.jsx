057a49fc691f5667b22c2b854bc8d34edba227db
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-04-13 22:03:48 +0100

    made header componant for nc news

---
+++ nc-news/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>North Coder News</h1>
+    </header>
+  );
+};
+
+export default Header;
