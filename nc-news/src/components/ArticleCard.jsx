9ca88c99340c445975943a415f4b3a24b674fe75
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-04-21 10:03:19 +0100

    added changes to logo and article card  as per feedback

--- nc-news/src/components/ArticleCard.jsx
+++ nc-news/src/components/ArticleCard.jsx
@@ -13,7 +13,10 @@ const ArticleCard = ({
 }) => {
   return (
     <Card style={{ margin: "10px" }}>
-      <Card.Header>{title}</Card.Header>
+      <Card.Header>
+        {" "}
+        <Link to={`/articles/${article_id}`}>{title}</Link>
+      </Card.Header>
       <Card.Body>
         <Card.Text>Topic: {topic}</Card.Text>
         <Card.Text>Comment Count: {comment_count}</Card.Text>
@@ -21,7 +24,6 @@ const ArticleCard = ({
         <ArticleVote votes={votes} article_id={article_id} type={"articles"} />
       </Card.Body>
       <Card.Footer className="text-muted">Created At: {created_at}</Card.Footer>
-      <Link to={`/articles/${article_id}`}>View More</Link>
     </Card>
   );
 };
--- nc-news/src/components/Header.jsx
+++ nc-news/src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import logo from "../image/logo.png";
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Image } from "react-bootstrap";
 
 const Header = (props) => {
   return (
     <Row className="header-background">
-      <Col className="text-center">
-        <Image src={logo} alt="Logo" fluid className="logo" />
-      </Col>
+      <Image src={logo} alt="Logo" fluid className="logo" />
     </Row>
   );
 };
